7207c8d5761af7c0aa59adeaf7c9d767328fc364
makarlistkov <makarmakaroshkin@mail.ru>
2025-03-28 17:24:23 +0300

    codestyle

--- app/src/main/java/com/example/mapsfriends/ui/components/MapScreen.kt
+++ app/src/main/java/com/example/mapsfriends/ui/components/MapScreen.kt
@@ -11,22 +11,24 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.mapsfriends.utils.MapViewModel
 import com.example.mapsfriends.R
 import com.example.mapsfriends.data.mockUsers
+import com.example.mapsfriends.ui.screens.MarkerData
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 @Composable
@@ -36,19 +38,12 @@ fun MapScreen(
 ) {
     val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
-
-    val currentUser = remember {
-        mockUsers.firstOrNull { it.id == 1 } ?: mockUsers.first()
-    }
-
+    val currentUser = remember { mockUsers.firstOrNull { it.id == 1 } ?: mockUsers.first() }
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(currentUser.location, 18f)
     }
 
-    LaunchedEffect(Unit) {
-        viewModel.loadMarkersIntoMap(context)
-    }
-
+    LaunchedEffect(Unit) { viewModel.loadMarkersIntoMap(context) }
     LaunchedEffect(cameraPositionState.position.zoom) {
         viewModel.updateMarkerIcons(cameraPositionState.position.zoom, context)
     }
@@ -67,28 +62,7 @@ fun MapScreen(
             )
         ) {
             viewModel.markers.forEach { markerData ->
-                markerData.icon?.let {
-                    Marker(
-                        state = MarkerState(position = markerData.position),
-                        icon = it,
-                        onClick = {
-                            if (viewModel.selectedMarkerId.value == markerData.id) {
-                                navController.navigate("profile/${markerData.id}")
-                                viewModel.selectedMarkerId.value = null
-                            } else {
-                                coroutineScope.launch {
-
-                                    cameraPositionState.animate(
-                                        CameraUpdateFactory.newLatLng(markerData.position),
-                                        150
-                                    )
-                                }
-                                viewModel.selectedMarkerId.value = markerData.id
-                            }
-                            true
-                        }
-                    )
-                }
+                CreateMapMarker(markerData, viewModel, navController, coroutineScope, cameraPositionState)
             }
         }
 
@@ -98,13 +72,47 @@ fun MapScreen(
                 .padding(end = 8.dp),
             onZoomChange = { newZoom ->
                 coroutineScope.launch {
-                    cameraPositionState.animate(
-                        CameraUpdateFactory.zoomTo(newZoom),
-                        150
-                    )
+                    cameraPositionState.animate(CameraUpdateFactory.zoomTo(newZoom), 150)
                 }
             },
             initialZoom = cameraPositionState.position.zoom
         )
+    }
+}
+
+@Composable
+private fun CreateMapMarker(
+    markerData: MarkerData,
+    viewModel: MapViewModel,
+    navController: NavHostController,
+    coroutineScope: CoroutineScope,
+    cameraPositionState: CameraPositionState
+) {
+    markerData.icon?.let {
+        Marker(
+            state = MarkerState(position = markerData.position),
+            icon = it,
+            onClick = { handleMarkerClick(markerData, viewModel, navController, coroutineScope, cameraPositionState) }
+        )
+    }
+}
+
+private fun handleMarkerClick(
+    markerData: MarkerData,
+    viewModel: MapViewModel,
+    navController: NavHostController,
+    coroutineScope: CoroutineScope,
+    cameraPositionState: CameraPositionState
+): Boolean {
+    return if (viewModel.selectedMarkerId.value == markerData.id) {
+        navController.navigate("profile/${markerData.id}")
+        viewModel.selectedMarkerId.value = null
+        true
+    } else {
+        coroutineScope.launch {
+            cameraPositionState.animate(CameraUpdateFactory.newLatLng(markerData.position), 150)
+        }
+        viewModel.selectedMarkerId.value = markerData.id
+        true
     }
 }
