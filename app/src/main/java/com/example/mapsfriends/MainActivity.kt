69e21437647d37b64bf02f43d30ac3c12f88cd3a
Surovets Elizaveta <surovets.elizaveta@gmail.com>
2025-04-07 00:16:34 +0300

    style fix 3

--- app/src/main/java/com/example/mapsfriends/MainActivity.kt
+++ app/src/main/java/com/example/mapsfriends/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.mapsfriends
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
