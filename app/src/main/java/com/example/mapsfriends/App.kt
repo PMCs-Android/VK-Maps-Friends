4b5f9e8ada01ae342582eba8f5276de6eb54c76d
Surovets Elizaveta <surovets.elizaveta@gmail.com>
2025-04-07 00:11:51 +0300

    style fix 2

--- app/src/main/java/com/example/mapsfriends/App.kt
+++ app/src/main/java/com/example/mapsfriends/App.kt
@@ -1,10 +1,13 @@
 package com.example.mapsfriends
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun App() {
     val navController = rememberNavController()
