539040c9d3f4c5b8a508c528a90b4891ddf02001
llsnqt <ewanprotopopov@yandex.ru>
2025-04-13 16:20:01 +0300

    fix codestyle

--- app/src/main/java/com/example/mapsfriends/MapViewModel.kt
+++ app/src/main/java/com/example/mapsfriends/MapViewModel.kt
@@ -10,7 +10,7 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.firestore.GeoPoint
 import kotlinx.coroutines.launch
 
-class MapViewModel() : ViewModel() {
+class MapViewModel : ViewModel() {
     val markers = mutableStateListOf<MarkerData>()
     val selectedMarkerId = mutableStateOf<String?>(null)
     val userRepository = FirebaseUserRepository()
@@ -22,7 +22,7 @@ class MapViewModel() : ViewModel() {
             friends?.forEach { user ->
                 val originalBitmap = loadOriginalBitmapFromUrl(context, user.avatarUrl)
                 originalBitmap?.let {
-                    val initialSize = calculateMarkerSize(18f) //start zoom
+                    val initialSize = calculateMarkerSize(18f) // start zoom
                     markers.add(
                         MarkerData(
                             id = user.userId,
